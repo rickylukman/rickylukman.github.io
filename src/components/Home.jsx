88c764d9c44ec146a7318a18a25d3d670ece4bfa
rickylukman <rickylukman0606@gmail.com>
2021-12-02 20:29:25 +0700

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Style/Home.scss'
+
+const Home = () => {
+    return (
+        <section id="Home">
+            <div className="container" id="home">
+                <div className="item">
+                    <h1>rickylukman/@cirricky</h1>
+                    <p>-it still under construction :)</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Home from './Home';
+
+function Main() {
+    return (
+        <Home/>
+    );
+}
+
+export default Main;
